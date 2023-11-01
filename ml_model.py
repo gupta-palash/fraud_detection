d4e6e7bce07fb2e8fed569e359d398578421b91e
gupta-palash <palash.05gupta@gmail.com>
2023-11-01 19:53:19 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
 print('Adding all ml realted coded here ')
+
+
+def model1():
+    print('adding code for ML Model1')
+
+def model2():
+    print('adding code for ML Model2')
+
+def model3():
+    print('adding code for ML Model3')
